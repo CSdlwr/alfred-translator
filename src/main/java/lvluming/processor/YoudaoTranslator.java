776bc685395ec2692dafa4c116cc849a7d0e60c4
lvluming <Lv.luming0858@gmail.com>
2017-08-14 22:09:52 +0800

    20170812_v2_spring: read apiUrl from config file using spring @value

--- src/main/java/lvluming/processor/YoudaoTranslator.java
+++ src/main/java/lvluming/processor/YoudaoTranslator.java
@@ -12,6 +12,7 @@ import lvluming.util.JsonUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author lvluming
@@ -21,7 +22,8 @@ public class YoudaoTranslator implements Handler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(YoudaoTranslator.class);
 
-    private static final String API_URL = "http://fanyi.youdao.com/openapi.do?keyfrom=alfred-llm-0858&key=425214878&type=data&doctype=json&version=1.1&";
+    @Value("${youdao.api.url}")
+    private String apiUrl;
 
     @Override
     public void handle(Request request, Response response) {
@@ -59,7 +61,7 @@ public class YoudaoTranslator implements Handler {
             }
         });
 
-        HttpRequest getRequest = Unirest.get(API_URL).queryString("q", query);
+        HttpRequest getRequest = Unirest.get(apiUrl).queryString("q", query);
         String url = getRequest.getUrl();
 
         LOGGER.info("api url = [{}]", url);
