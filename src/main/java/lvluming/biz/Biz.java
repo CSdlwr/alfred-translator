9a0bd042600001cc792abef08e1efa061133c060
lvluming <Lv.luming0858@gmail.com>
2017-08-15 22:40:04 +0800

    master: re-arch project, add transport role

---
+++ src/main/java/lvluming/biz/Biz.java
@@ -0,0 +1,18 @@
+package lvluming.biz;
+
+/**
+ * @author lvluming
+ * @date 2017/8/15 22:36
+ */
+public class Biz {
+
+    private String query;
+
+    static {
+
+    }
+
+    private Biz(String query) {
+        this.query = query;
+    }
+}
