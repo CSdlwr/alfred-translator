b3869c1615011513287a587782bbb198943446c4
lvluming <Lv.luming0858@gmail.com>
2017-08-17 22:31:58 +0800

    master: planning HttpClient

--- src/main/java/lvluming/util/HttpClient.java
+++ src/main/java/lvluming/util/HttpClient.java
@@ -1,8 +1,18 @@
 package lvluming.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author lvluming
  * @date 2017/8/6 00:04
  */
 public class HttpClient {
+
+    public static String get(String url) {
+        return StringUtils.EMPTY;
+    }
+
+    public static <T> T getObject(String url) {
+        return null;
+    }
 }
